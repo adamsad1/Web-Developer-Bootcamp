5c5cea3bed244ef99766ff1d02ed958f79ab543a
adamsad1 <adrian.k.adams@gmail.com>
2018-02-25 15:07:28 -0500

    Lecture 98 - Conditionals

--- scratch.js
+++ scratch.js
@@ -1,3 +1,21 @@
-var userName = prompt("What is your name?");
-alert("Nice to meet you " + userName);
-console.log("Also great to meet you, " + userName);
+var age = prompt("What is your age?");
+
+if (age < 0){
+    console.log("Error");
+} else if (age < 18) {
+    console.log("You cannot enter");
+} else if (age < 21) {
+    console.log("You cannot drink");
+} else if (age == 21) {
+    console.log("happy 21st birthday");
+} else {
+    console.log("you may enter");
+}
+
+if (age % 2 !== 0) {
+    console.log("your age is odd");
+}
+
+if (Math.sqrt(age) % 1 == 0){
+    console.log("perfect square");
+}
