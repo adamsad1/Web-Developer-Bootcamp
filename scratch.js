1b160ecfb581cbf2c948d07d9d219e89fc29750c
adamsad1 <adrian.k.adams@gmail.com>
2018-03-04 20:15:14 -0500

    added delete and loop for list

--- scratch.js
+++ scratch.js
@@ -5,10 +5,23 @@ window.setTimeout(function() {
 
     while(input !== "quit") {
         if(input === "list") {
-            console.log(todos);
+            console.log("**********");
+            for(var i = 0; i < todos.length; i++){
+                console.log(i + ": " + todos[i]);
+            }
+            console.log("**********");
         } else if (input === "new") {
             var newTodo = prompt("Enter new todo");
             todos.push(newTodo);
+            console.log(newTodo + " added to list");
+        } else if (input === "delete") {
+            var deleteTodo = prompt("Enter todo to delete");
+            if(deleteTodo >= todos.length || deleteTodo < 0) {
+                console.log("Not a valid todo");
+            } else {
+                todos.splice(deleteTodo);
+                console.log("Todo " + deleteTodo + " deleted");
+            }
         }
         input = prompt("What would you like to do?");
     }
