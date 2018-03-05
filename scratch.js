26aa277877701bb2ff8379c0aa4048286364ea5c
adamsad1 <adrian.k.adams@gmail.com>
2018-03-04 21:44:09 -0500

    refectored into functions

--- scratch.js
+++ scratch.js
@@ -1,30 +1,42 @@
-window.setTimeout(function() {
+window.setTimeout(function () {
     var todos = []
 
     var input = prompt("What would you like to do?");
 
-    while(input !== "quit") {
-        if(input === "list") {
-            console.log("**********");
-            for(var i = 0; i < todos.length; i++){
-                console.log(i + ": " + todos[i]);
-            }
-            console.log("**********");
+    while (input !== "quit") {
+        if (input === "list") {
+            listTodos();
         } else if (input === "new") {
-            var newTodo = prompt("Enter new todo");
-            todos.push(newTodo);
-            console.log(newTodo + " added to list");
+            addTodo();
         } else if (input === "delete") {
-            var deleteTodo = prompt("Enter todo to delete");
-            if(deleteTodo >= todos.length || deleteTodo < 0) {
-                console.log("Not a valid todo");
-            } else {
-                todos.splice(deleteTodo);
-                console.log("Todo " + deleteTodo + " deleted");
-            }
+            deleteTodo();
         }
         input = prompt("What would you like to do?");
     }
 
+    function listTodos() {
+        console.log("**********");
+        todos.forEach(function (todo, index) {
+            console.log(index + ": " + todo);
+        })
+        console.log("**********");
+    }
+
+    function addTodo() {
+            var newTodo = prompt("Enter new todo");
+            todos.push(newTodo);
+            console.log(newTodo + " added to list");
+    }
+
+    function deleteTodo() {
+            var targetTodo = prompt("Enter todo to delete");
+            if (targetTodo >= todos.length || targetTodo < 0) {
+                console.log("Not a valid todo");
+            } else {
+                todos.splice(targetTodo);
+                console.log("Todo " + targetTodo + " deleted");
+            }
+    }
+    
     console.log("Okay, you quit the app");
 }, 500);
